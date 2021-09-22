25725a3e98131909e389fef788c801a736e95583
JatinsticGaming <62481751+JatinsticGaming@users.noreply.github.com>
2021-09-22 18:46:30 +0530

    Working on FEN Parsing

--- src/ChessEngine.cpp
+++ src/ChessEngine.cpp
@@ -4,14 +4,20 @@
 #include "UCI/UCIParser.h"
 #include "UCI/UCIExecutor.h"
 #include "UCI/Options/Options.h"
+#include "Game Visualisation/Board.h"
 
 using namespace std;
+using namespace Game;
 
 map<string, string> options;
 
 int main(){
     UCI::Options::init(&options);
     UCI::Executor::init();
+
+    Board board;
+    board.load("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
+    board.print();
 
     string cmd;
     getline(cin, cmd);
